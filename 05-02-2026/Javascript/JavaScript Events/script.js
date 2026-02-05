88fefa3c5bf8f2cbe8f4a9bfd9755dc9be48d993
Keerthivasak-Sparkout <keerthivasank@sparkouttech.com>
2026-02-05 19:24:17 +0530

    change in style and script file

--- 05-02-2026/Javascript/JavaScript Events/script.js
+++ 05-02-2026/Javascript/JavaScript Events/script.js
@@ -1,3 +1,24 @@
-document.addEventListener("mousemove", function (event) {
-  console.log("Mouse position:", event.clientX, event.clientY);
+const form = document.getElementById("form");
+const tableBody = document.querySelector("#dataTable tbody");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+  const gender = document.querySelector('input[name="gender"]:checked');
+  const language = document.getElementById("lang").value;
+
+  if (!gender) {
+    alert("Please select gender");
+    return;
+  }
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>${name}</td>
+    <td>${age}</td>
+    <td>${gender.value}</td>
+    <td>${language}</td>
+  `;
+  tableBody.appendChild(newRow);
+  form.reset();
 });
